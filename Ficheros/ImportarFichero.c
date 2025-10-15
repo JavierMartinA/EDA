6fcdce83dd6e590ef237eac560eee571f4725f13
Javier Martin <9404008@alumnos.ufv.es>
2025-10-15 09:56:06 +0200

    Import

--- Ficheros/ImportarFichero.c
+++ Ficheros/ImportarFichero.c
@@ -16,6 +16,81 @@
 void ImportarFichero(DISCO **Fichas,WINDOW *Wfichero,bool sumar)
 {
    
+    #include "Ficheros.h"
+
+    char nombreFicheros[50];
+
+    FILE *punteroFichero;
+
+    long inicio;
+    long final;
+    int tim;
+
+    char obra[50];
+    char apellidos[50];
+    char nombre[50];
+    char tonalidad[50];
+    char opus[50];
+    char duracion[50];
+
+    int contador;
+
+
     // Código del alumno
-    
+    if (!sumar) //No debería haber discos
+    {
+        if (Estadisticas.NumeroFichas == '0') //Comprobación de 0 discos
+        {
+            touchwin(Wfichero);
+            wrefresh(Wfichero);
+
+            wscanw(Wfichero,"%s",&nombreFicheros);
+
+            fopen(nombreFicheros, "r"); //Cabecera en línea 0
+
+            if (punteroFichero == NULL)
+            {
+                VentanaError("No se ha encontrado el fichero");
+                return;
+            }
+
+            
+            inicio = clock();
+            fscanf(punteroFichero, "%s; %s; %s;%s;%s;%s"); //Para quitar header
+
+            while (fscanf(punteroFichero, "%s; %s; %s;%s;%s;%s"), &obra, &apellidos, &nombre, &tonalidad, &opus, &duracion)
+            {
+                contador++;
+
+            }
+            
+
+
+            
+
+
+        }
+
+        else
+        {
+            VentanaError("Hay fichas de discos");
+            return;
+        }
+        
+        
+    }
+
+    else
+    {
+        if(Estadisticas.NumeroFichas > 0 || Estadisticas.NumeroFichas < 0) //Comprobación de que hay discos
+        {
+
+        }
+
+        else
+        {
+            VentanaError("No hay fichas de discos");
+        }
+    }
+
 }
