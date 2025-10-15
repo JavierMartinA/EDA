7ec890a14e4ef9ca88d9aedaf8e172dfaa4c34a0
Javier Martin <9404008@alumnos.ufv.es>
2025-10-15 08:31:47 +0200

    Inicio

---
+++ Ficheros/ImportarFichero.c
@@ -0,0 +1,21 @@
+/*****************************************
+ * Nombre: ImortarFichero
+ * Argumentos: DISCO **Fichas:   Puntero al array de discos
+ *             WINDOW *Wfichero: Ventana para capturar el nombre del fichero
+ *             bool sumar:       Si es true las fichas de discos se añaden a las existentes,
+ *                               si es false la lista de discos tiene que estar vacía.
+ * Descripción: Lee el nombre del fichero que contendrá los datos a importar. Carga los discos del fichero
+ *              en el array de discos, dimensionándolo si es necesario y descartando los que no tengan título o autor.
+ * Reglas de uso: 
+ * Código de Retorno:
+ * Programador: JMSM (Sept-24)
+ *****************************************/
+
+#include "Ficheros.h"
+
+void ImportarFichero(DISCO **Fichas,WINDOW *Wfichero,bool sumar)
+{
+   
+    // Código del alumno
+    
+}
